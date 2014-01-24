9c3e6de063f6cf6c587850b14514fb707281a986
drak@kaverne <drak@kaverne>
2014-01-24 21:39:37 +0100

    longer and stronger introduction.

--- src/de/saces/fnplugins/SCGIPublisher/SCGIServer.java
+++ src/de/saces/fnplugins/SCGIPublisher/SCGIServer.java
@@ -336,10 +336,10 @@ public class SCGIServer extends AbstractServer implements AbstractService {
 			body.append(errorText);
 			body.append("</p><p>");
 		}
-		body.append("This gateway is for demonstration purposes only. You can only request the Freenet URIs listed below.");
+		body.append("This is a restricted proxy into Freenet. You can only request the URIs listed below. All the content you can access belongs only to its creators. We only take responsibility in so far that we ensured that no URI listed here contained any illegal material when we checked it. Should you find illegal on any site you can access via this proxy, please notify us instantly so we can remove the URI from our whitelist!");
 		body.append("</p><p>");
-		body.append("To get the full satisfaction install <a href=\"http://freenetproject.org\">Freenet</a> and request the Freenet URIs on your own node without the limits here.");
-		body.append("</p><p>Valid Freenet URIs:<ul>");
+		body.append("To get access to all content, install <a href=\"http://freenetproject.org\">Freenet</a> and request the Freenet URIs on your own node without the limits here.");
+		body.append("</p><p>Allowed Freenet URIs (whitelist):<ul>");
 
 		for (Filter filter: filters) {
 			if (filter instanceof StrictFilter) {
